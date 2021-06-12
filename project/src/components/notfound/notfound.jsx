4bfe3a98da29f048cc592ad0e6022c29acf60aa6
Diana Bobrova <diana.bobrova.spb@gmail.com>
2021-06-13 02:27:48 +0300

    Создаю страницу 404

--- project/src/components/notfound/notfound.jsx
+++ project/src/components/notfound/notfound.jsx
@@ -1,5 +1,21 @@
-import React from 'react';
+import React from "react";
 
 export default function NotFound() {
-    return;
+    const notFoundStyle = {
+        display: "white",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      };
+  return (
+    <section className="page page--gray page--main" style={notFoundStyle}>
+      <h1>404. Page not found</h1>
+      <img
+        src="img/404.svg"
+        alt="Well then"
+        width="250"
+        height="202"
+      />
+    </section>
+  );
 }
