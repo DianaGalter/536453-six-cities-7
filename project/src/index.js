d71f9a1bed6adf575a2cbf3081647f9788ef2536
Diana Bobrova <diana.bobrova.spb@gmail.com>
2021-07-14 00:06:45 +0300

    Добавляю провайдер в индекс

--- project/src/index.js
+++ project/src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import {reducer} from './store/reducer';
 import App from './components/app/app';
 import offers from './mocks/offers';
+import {composeWithDevTools} from 'redux-devtools-extension';
+
+const store = createStore(reducer, composeWithDevTools());
 
 ReactDOM.render(
   <React.StrictMode>
-    <App offers={offers}/>
+    <Provider store={store}>
+      <App offers={offers}/>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
